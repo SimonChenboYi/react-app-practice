6cde695a68e3ef9558e0742de603bbe61a9e1c6e
Simon Chenbo Yi <yichenbo@outlook.com>
2024-01-03 12:03:32 +0000

    basic list group and enable typescript formatter

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,7 @@
-
 function Message() {
-  const name = 'Simon';
-  if (name)
-    return <h1>Hello {name}</h1>;
+  const name = "Simon";
+  if (name) return <h1>Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
 
-export default Message; 
+export default Message;
