5c72bde99e95c7e33b7809f8bfa4f132ed50e96e
Simon Chenbo Yi <yichenbo@outlook.com>
2024-01-03 14:40:35 +0000

    type annotation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,9 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["London", "Brimingham", "Manchester", "York", "Edingburgh"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -7,11 +11,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event, item, index)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
