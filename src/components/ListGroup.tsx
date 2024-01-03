cb9773d5061db79fce39749f28ecc4397f265758
Simon Chenbo Yi <yichenbo@outlook.com>
2024-01-03 14:35:19 +0000

    some class for css and onclick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 function ListGroup() {
   let items = ["London", "Brimingham", "Manchester", "York", "Edingburgh"];
-  items = [];
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event, item, index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
